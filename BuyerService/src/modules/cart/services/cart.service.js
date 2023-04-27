fe2c650d901947164defdb04d62ca4f1159479a4
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2023-04-27 12:28:46 +0530

    Complete endpoints for cart

--- BuyerService/src/modules/cart/services/cart.service.js
+++ BuyerService/src/modules/cart/services/cart.service.js
@@ -1,48 +1,41 @@
-import { insertCart, getCartByBuyerId, updateCartByBuyerId, deleteCartByBuyerId} from '../repositories/cart.repository';
+import { insertCart, getCartByBuyerId, updateCartByBuyerId, deleteCartByBuyerId } from '../repositories/cart.repository';
 
-export const createCart = async (userId, data) => {
-  return insertCart({ ...data, buyerId: userId })
+export const createCart = async (buyerId, data) => {
+  const cart = await getCartByBuyerId(buyerId);
+
+  if (cart) {
+    return { status: 400, message: 'Cart already exists!' };
+  }
+
+  await insertCart({ ...data, buyerId: buyerId });
+
+  return { status: 200, data: cart };
 };
 
-export const retrieveCart = async (userId) => {
-  let cart = await getCartByBuyerId(userId);
+export const retrieveCart = async (buyerId) => {
+  const cart = await getCartByBuyerId(buyerId);
+  console.log('Cart = ', cart);
 
   if (!cart) {
-    return { status: 400, message: "Cart doesn't exist to update" };
+    return { status: 404, message: "Cart doesn't exist to update" };
   }
-}
+  return { status: 200, data: cart };
+};
 
-export const updateCartById = async (cart_id, data, user) => {
-  let cart = await findCart({ _id: cart_id })
-  if (!cart) return { status: 400, message: "Cart doesn't exist to update" }
-  if (data.name) {
-    const check = await findCart({ name: data.name })
-    if (check && check._id?.toString() !== cart_id?.toString()) return { status: 400, message: 'Cart name already taken' }
-  }
-  if (cart.creator_lock && cart.creator.toString() !== user._id.toString()) return { status: 403, message: 'You are not authorized to update this cart' }
-  if (data.max_score) {
-    const r = await getSubmissions({ filter: { cart: cart_id } }).then((res) => {
-      return res
-    })
-    if (r.totalDocs > 0) return { status: 400, message: 'Cannot update cart with submissions' }
-  }
+export const updateCartById = async (buyerId, data) => {
+  const cart = await getCartByBuyerId();
 
-  return await findAndUpdateCart({ _id: cart_id }, data)
-}
+  if (!cart) return { status: 404, message: "Cart doesn't exist to update" };
 
-export const deleteCart = async (cart_id, user) => {
-  const cart = await findCart({ _id: cart_id })
-  const checkSubmission = await getOneSubmission({ cart: cart_id })
+  await updateCartByBuyerId(buyerId, data);
 
-  if (cart.enabled) {
-    return { status: 400, message: 'Failed to delete cart/ Cart is active' }
-  }
+  return { status: 200, data: cart };
+};
 
-  if (checkSubmission) {
-    return { status: 400, message: 'Failed to delete cart/ Cart already has a submission' }
-  }
+export const deleteCart = async (buyerId) => {
+  const cart = await getCartByBuyerId();
 
-  if (!cart) return { status: 400, message: "Cart doesn't exist to remove" }
-  if (cart.creator_lock && cart.creator.toString() !== user._id.toString()) return { status: 403, message: 'You are not authorized to delete this cart' }
-  return await deleteACart({ _id: cart_id })
-}
+  if (!cart) return { status: 404, message: "Cart doesn't exist to remove" };
+
+  return deleteCartByBuyerId(buyerId);
+};
