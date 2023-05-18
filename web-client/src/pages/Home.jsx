015898b0863a474f78565f8961fc48bed0fba4c4
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2023-05-19 01:06:18 +0530

    Add tailwind css

--- web-client/src/pages/Home.jsx
+++ web-client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="font-bold text-5xl">Home</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
