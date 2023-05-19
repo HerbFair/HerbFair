aa0a7add175745ef772ea5997dd16f37db60c697
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2023-05-19 19:09:01 +0530

    Add navbar and site logo

--- web-client/src/App.jsx
+++ web-client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import MainRoutes from "./routes/MainRoutes";
+import { NavBar } from "./components";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar />
         <MainRoutes />
       </BrowserRouter>
     </div>
