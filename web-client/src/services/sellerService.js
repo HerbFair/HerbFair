6ac8b723d1b92333a2a342e6d80e7591a30e1ed4
Pasindu Lakshan <dkp9912@gmail.com>
2023-05-20 11:33:51 +0530

    Fix

--- web-client/src/services/sellerService.js
+++ web-client/src/services/sellerService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:8090/api/user-service/v1";
+const BASE_URL = "http://localhost:4008/api/user-service/v1";
 
 class SellerService {
   //Seller Login
